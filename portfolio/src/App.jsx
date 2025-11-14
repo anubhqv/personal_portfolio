5da0ef917c9e307f8a06ed1b04ae0285dddb9977
kulbhooshan singh <anubhavsinghrathore45@gmail.com>
2025-11-14 22:43:25 +0530

    made feature card

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
@@ -8,6 +9,7 @@ const App=()=>{
         <NavBar />
         <Hero />
         <ShowcaseSection/>
+        <FeatureCards   />
         </>
     );
 }
---
+++ portfolio/src/sections/FeatureCards.jsx
@@ -0,0 +1,20 @@
+import { abilities } from '../constants'
+
+const FeatureCards = () => {
+  return (
+    <div className='w-full padding-x-lg'>
+        <div className='mx-auto grid-3-cols'>
+            {abilities.map(({imgPath, title, desc})=>(
+                <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+                    <div className='size-14 flex items-center rounded-full'>
+                        <img src={imgPath} alt={title}/>
+                    </div>
+                   
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeatureCards
