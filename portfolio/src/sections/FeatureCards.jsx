675bcf04e95458f542a5c885cea3eee26e56999f
kulbhooshan singh <anubhavsinghrathore45@gmail.com>
2025-11-14 22:44:09 +0530

    completed changes

--- portfolio/src/sections/FeatureCards.jsx
+++ portfolio/src/sections/FeatureCards.jsx
@@ -9,7 +9,8 @@ const FeatureCards = () => {
                     <div className='size-14 flex items-center rounded-full'>
                         <img src={imgPath} alt={title}/>
                     </div>
-                   
+                    <h3 className='text-white text-2xl font-semibold mt-2'>{title}</h3>
+                    <p className='text-white-50 text-lg'>{desc}</p>
                 </div>
             ))}
         </div>
